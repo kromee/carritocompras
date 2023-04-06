850fa4dfa800c983fb40c188ea31ca2e49a7aa73
eduardo dominguez <lalo_wincaja@hotmail.com>
2023-04-06 13:33:09 -0600

    proyecto inicial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
+import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+class App extends Component {
+  state={
+    productos: [
+      {name:'Tmate',price:10,img:'images/tomate.jpg'},
+      {name:'Arbejas',price:10,img:'images/arbejas.jpg'},
+      {name:'Lechuga',price:10,img:'images/lechuga.jpg'},
+    ]
+  }
+  render(){
+    return (
+      <div>
+      <p>Hola mundo </p>
     </div>
-  );
+    )
+
+  }
 }
 
 export default App;
