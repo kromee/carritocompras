8a9b6732c384f8bcee0d8a0fa6d7367111292da5
kromee <renixedu@gmail.com>
2023-04-12 18:16:23 -0600

    estilos y button
    se da estilos al cuadro de la imagen y botones

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Productos from './Components/Productos'
 
 
 class App extends Component {
   state={
     productos: [
-      {name:'Tmate',price:10,img:'images/tomate.jpg'},
-      {name:'Arbejas',price:10,img:'images/arbejas.jpg'},
-      {name:'Lechuga',price:10,img:'images/lechuga.jpg'},
+      {name:'Tmate',price:11,img:'/images/tomate.jpg'},
+      {name:'Arbejas',price:12,img:'/images/arbejas.jpg'},
+      {name:'Lechuga',price:13,img:'/images/lechuga.jpg'},
     ]
   }
   render(){
     return (
       <div>
-      <p>Hola mundo </p>
+        <Productos agregarAlCarro={()=>console.log('no hace nada')} 
+        productos = {this.state.productos}
+        />
+        
     </div>
     )
 
